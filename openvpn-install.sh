27d9cb60a04d96c9215b563df46c42227d6ea6a2
Element <my-tangjianbin@163.com>
2019-02-23 18:11:06 +0800

    change openvpn-plugin-auth-pam.so file and to test script

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -335,8 +335,8 @@ verb 3
 crl-verify crl.pem
 client-to-client" >> /etc/openvpn/server.conf
 	if [[ "$GOOGLEAUTH" = '1' ]]; then
-		echo "plugin /lib64/security/pam_google_authenticator.so openvpn" >> /etc/openvpn/server.conf
-		#"plugin /usr/lib64/openvpn/plugins/openvpn-plugin-auth-pam.so openvpn" >> /etc/openvpn/server.conf
+		#echo "plugin /lib64/security/pam_google_authenticator.so openvpn" >> /etc/openvpn/server.conf
+		echo "plugin /usr/lib64/openvpn/plugins/openvpn-plugin-auth-pam.so openvpn" >> /etc/openvpn/server.conf
 	fi
 	# Enable net.ipv4.ip_forward for the system
 	echo 'net.ipv4.ip_forward=1' > /etc/sysctl.d/30-openvpn-forward.conf
